820640574331e9ffafb59e140953af0869c38954
Daniel Gorbatov <dzgorbatov@gmail.com>
2022-08-06 23:08:12 -0700

    fix page go back

--- components/editorpages/sushiscouts/method.tsx
+++ components/editorpages/sushiscouts/method.tsx
@@ -28,10 +28,10 @@ const ScoutingMethods: NextPage<PropsData> = (props: PropsData) => {
     }, [currPage]);
 
     return <>
-        <section className={styles.title}>
-            <svg width="20" height="22" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.back} onClick={() => props.setPage("Sushi Scouts")}>
+        <section className={styles.title} onClick={() => console.log("cclicked")}>
+            <div onClick={() => {console.log("hi"); props.setPage("Sushi Scouts");}}><svg width="20" height="22" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.back}>
                 <path d="M2.31234 13.5756C0.3634 12.4116 0.363405 9.58844 2.31235 8.42441L15.2117 0.720152C17.2114 -0.474169 19.75 0.966562 19.75 3.29574V18.7043C19.75 21.0334 17.2114 22.4742 15.2117 21.2798L2.31234 13.5756Z" fill="black"/>
-            </svg>
+            </svg></div>
 
             <h1>{props.method}</h1>
         </section>
