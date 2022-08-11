0ae7eb1556df70f7f32eb752ad3a0367075b17aa
Daniel Gorbatov <dzgorbatov@gmail.com>
2022-08-10 20:01:13 -0700

    cors???

--- pages/api/getconfigfile.ts
+++ pages/api/getconfigfile.ts
@@ -1,11 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import {prisma} from "../../util/prisma";
+import Cors from 'cors';
+
+const cors = Cors({
+    methods: [ 'GET'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+    req: NextApiRequest,
+    res: NextApiResponse,
+    fn: Function
+  ) {
+    return new Promise((resolve, reject) => {
+      fn(req, res, (result: any) => {
+        if (result instanceof Error) {
+          return reject(result)
+        }
+  
+        return resolve(result)
+      })
+    })
+  }
+  
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await runMiddleware(req, res, cors);
+
   try {
       const teamNum = req.query["teamNum"];
       const year = req.query["year"];
