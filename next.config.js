9b14cbf2fcbff815637073a0d4935c76291fc796
Daniel Gorbatov <dzgorbatov@gmail.com>
2022-08-14 23:34:22 -0700

    fix cors?

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://sushi-structeres.herokuapp.com/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
